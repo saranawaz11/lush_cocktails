70ca12c7148dbae23c8b7b97aa645035b6c9a3c9
saranawaz11 <saranawaz852@gmail.com>
2025-11-16 02:31:18 +0500

    first commit

---
+++ app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./_components/nabar";
+
+const Home = () => {
+  return ( 
+    <div className="flex-center h-screen">
+      <h1 className="text-2xl text-green-950 font-extrabold">GSAP APP</h1>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default Home;
